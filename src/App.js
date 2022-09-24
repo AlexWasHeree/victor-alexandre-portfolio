7a7c93fe331226b17bb3b30d8162e692f9046c71
AlexWasHeree <victoralexandre2303@gmail.com>
2022-09-24 14:41:10 -0300

    font imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from 'react';
+import './App.css';
+
 function App() {
-  return <div className="App">Meu app</div>;
+  return (
+    <div className="App">
+      <h1 className="titulo">titulo</h1>
+      <h2 className="subtitulo">subtitulo</h2>
+      <h3 className="terceiroTitulo">terceiroTitulo</h3>
+      <p className="body">body</p>
+      <p className="funcional">funcional</p>
+      <p className="subFuncional">subFuncional</p>
+    </div>
+  );
 }
 
 export default App;
